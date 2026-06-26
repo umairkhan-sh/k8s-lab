7d091d54b854d8bd0b6659f9c47e7db2aead6165
umair <umair.sh@protonmail.com>
2026-06-26 17:04:13 +0500

    ADDED cilium helm install for node readiness

--- local/kind-bootstrap.sh
+++ local/kind-bootstrap.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+
 kind create cluster --config kind-config.yaml
+
+helm install cilium cilium/cilium --version 1.19.5 \
+  --namespace kube-system \
+  --set kubeProxyReplacement=true \
+  --set k8sServiceHost=k8s-lab-external-load-balancer \
+  --set k8sServicePort=6443
 
 flux bootstrap github \
   --owner=$GITHUB_USER \
